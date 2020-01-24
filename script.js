1046d253d7f44ece3b67bd212e1652fca376cb3a
emilie <emi.m.humbert@googlemail.com>
2020-01-24 17:46:52 +1300

    add zip code with live validation

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const form  = document.getElementsByTagName('form')[0];
 
+
+// email
 const email = document.getElementById('mail');
 const emailError = document.querySelector('#mail + span.error');
 
@@ -8,19 +10,19 @@ email.addEventListener('input', function (event) {
     emailError.innerHTML = ''; 
     emailError.className = 'error';
   } else {
-    showError();
+    showEmailError();
   }
 });
 
 form.addEventListener('submit', function (event) {
   if(!email.validity.valid) {
-    showError();
+    showEmailError();
 
     event.preventDefault();
   }
 });
 
-function showError() {
+function showEmailError() {
   if(email.validity.valueMissing) {
     emailError.textContent = 'Please enter an e-mail address.';
   } else if(email.validity.typeMismatch) {
@@ -30,4 +32,39 @@ function showError() {
   }
 
   emailError.className = 'error active';
+}
+
+
+// zipcode
+
+const zipCode = document.getElementById('zip-code');
+const zipCodeError = document.querySelector('#zip-code + span.error');
+
+zipCode.addEventListener('input', function (event) {
+  if (zipCode.validity.valid) {
+    zipCodeError.innerHTML = ''; 
+    zipCodeError.className = 'error';
+  } else {
+    showZipError();
+  }
+});
+
+form.addEventListener('submit', function (event) {
+  if(!zipCode.validity.valid) {
+    showZipError();
+
+    event.preventDefault();
+  }
+});
+
+function showZipError() {
+  if(zipCode.validity.valueMissing) {
+    zipCodeError.textContent = 'Please enter your zip code.';
+  } else if(zipCode.validity.patternMismatch) {
+    zipCodeError.textContent = 'Entered value needs to be a zip code (e.g. 55555 or 55555-5555)';
+  } else if(zipCode.validity.tooShort) {
+    zipCodeError.textContent = `Zip code should be at least ${ zipCode.minLength } characters; you entered ${ zipCode.value.length }.`;
+  }
+
+  zipCodeError.className = 'error active';
 }
